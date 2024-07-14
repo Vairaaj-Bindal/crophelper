018635ff1f013d8d02648cdd7bb51c3ea23bd697
Vairaaj-Bindal <107085242+Vairaaj-Bindal@users.noreply.github.com>
2024-07-13 17:02:29 -0700

    Update cropapp.py

--- cropapp.py
+++ cropapp.py
@@ -4,7 +4,7 @@ from sklearn import tree
 from sklearn import metrics
 
 # Load the dataset (replace 'crop_data.csv' with your actual dataset file)
-data = pd.read_csv('Crop_recommendation 2.csv')
+data = pd.read_csv('/Users/tript/Downloads/Crop_recommendation 2.csv')
 
 # Separate features (soil and environmental parameters) and target (crop categories)
 X = data[['N', 'P', 'K', 'temperature', 'ph', 'humidity', 'rainfall']]
